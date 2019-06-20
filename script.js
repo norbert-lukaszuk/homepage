5985c508ca1d922bd0624f5c44ddf554a880f3f7
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2019-06-20 12:22:02 +0200

    js changing html

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-console.log('Witam na mojej stronie nauki FrontEndu. Pozdro!!')
+console.log('Witam na mojej stronie nauki FrontEndu. Pozdro!!');
+const secondHeading=document.querySelectorAll('.secondHeader');
+console.log(secondHeading[2]);
+secondHeading[2].innerHTML = 'Jest to sekcja stworzona przy użyciu JS!';
