df9de4daa3cd76f96ffdd972afee04c33b320a78
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2019-07-07 14:08:44 +0200

    feat: hamburger menu

---
+++ scripts/famousPeople.js
@@ -0,0 +1,5 @@
+const menuButton = document.querySelector('.menu__button--js')
+menuButton.addEventListener('click',(e)=>{
+    const navigationList = document.querySelector('.navigationList--js');
+    navigationList.classList.toggle('navigationList--visible');
+});
