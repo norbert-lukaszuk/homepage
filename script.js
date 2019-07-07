1376972a9e44b123e50d1113758f204d9c58b366
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2019-07-07 10:41:07 +0200

    new page: famous people of computer era

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 console.log('Witam na mojej stronie nauki FrontEndu. Pozdro!!');
 const secondHeading=document.querySelectorAll('.secondHeader');
 console.log(secondHeading[2]);
-secondHeading[2].innerHTML = 'Jest to sekcja stworzona przy użyciu JS!';
+//secondHeading[2].innerHTML = 'Jest to sekcja stworzona przy użyciu JS!';
+
+function calculate(x){
+    return x*2;
+    
+}
+
+console.log(calculate(5));
+const myFun=calculate(5);
+console.log(myFun);
