6acfbfa5adadee63c37a0c55b05e0e25605ce0f4
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2019-07-08 22:27:37 +0200

    new pages look

--- scripts/famousPeople.js
+++ scripts/famousPeople.js
@@ -2,4 +2,6 @@ const menuButton = document.querySelector('.menu__button--js')
 menuButton.addEventListener('click',(e)=>{
     const navigationList = document.querySelector('.navigationList--js');
     navigationList.classList.toggle('navigationList--visible');
+   
+    
 });
