1e9cc75d97fdeae868a1c10e484f5deccf8e1891
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2019-09-09 10:57:37 +0200

    feat: selecting text

--- script.js
+++ script.js
@@ -7,7 +7,19 @@ function calculate(x){
     return x*2;
     
 }
-
+newArray=new Array();
+let counter=0;
+let element;
 console.log(calculate(5));
 const myFun=calculate(5);
 console.log(myFun);
+myArray=new Array();
+for(i=0;i<=10;i++){localStorage.setItem(i, i+3/5 )};
+for(i=0;i<=10;i++){/*element = localStorage.getItem(i);*/
+    // console.log(element);
+    myArray.push(localStorage.getItem(i));
+    
+}
+console.log(myArray);
+let selectedText = document.getSelection.toString();
+// let textString = selectedText.toString();
